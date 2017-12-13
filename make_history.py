81ef4d5d55e2dc1bd80f9bd441b03a9807fbcd90
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-12-13 17:50:35 -0600

    Extract Meetup event history from Meetup api

---
+++ make_history.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os
+import meetup.api
+import pandas
+import pypandoc
+
+key = os.environ['MEETUP_API_KEY']
+client = meetup.api.Client(key)
+group = client.GetGroup({'urlname': 'MadPython'})
+events = client.GetEvents({'group_id': group.id, 'status': 'past'})
+data = pandas.DataFrame.from_records(events.results)
+data.rename(columns={'name': 'event_name'}, inplace=True)
+event_names = data.apply(lambda r: f'* [{r.event_name}]({r.event_url})', axis=1)
+with open('history.md', 'w') as f:
+    f.write('\n'.join(event_names.tolist()))
