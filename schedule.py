6de53d05d2e8a5be5f0dde68317ba80f7c9b761d
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-03-17 08:00:27 -0500

    Forked madison-python/history for upcoming events

--- schedule.py
+++ schedule.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 import os
+from datetime import datetime
 import meetup.api
 import pandas
-import pypandoc
 
 key = os.environ['MEETUP_API_KEY']
 client = meetup.api.Client(key)
 group = client.GetGroup({'urlname': 'MadPython'})
-events = client.GetEvents({'group_id': group.id, 'status': 'past'})
+events = client.GetEvents({'group_id': group.id, 'status': 'upcoming'})
 data = pandas.DataFrame.from_records(events.results)
 data.rename(columns={'name': 'event_name'}, inplace=True)
-event_names = data.apply(lambda r: f'* [{r.event_name}]({r.event_url})', axis=1)
+data['date'] = data.time.apply(
+        lambda ms: datetime.fromtimestamp(ms/1000))
+data['datef'] = data.date.apply(
+        lambda dt: datetime.strftime(dt, '%a %b %d, %Y'))
+
+event_names = data.apply(lambda r: f'* {r.datef}: [{r.event_name}]({r.event_url})', axis=1)
 with open('README.md', 'w') as f:
     f.write('\n'.join(event_names.tolist()))
